99971f970bc0de706e3edb76ed7830f27dadad33
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-12-29 17:06:25 +0900

    CH02

---
+++ src/Ch01/C01HelloWorld.java
@@ -0,0 +1,18 @@
+package Ch01;//패키지명(폴더와 유사)
+//;=문장의 마침표역할
+
+public class C01HelloWorld//클래스 영역-객체 지향 문법 적용 단위 
+{
+	//메서드(방법,기능):작업수행을 하는데 필요한 여러코드를 묶어서 실행하는데 사용되는 단위
+	//MAIN 메서드		:최초 실행 함수
+	//library 메서드	:미리 만들어져서 제공되는 함수
+	//사용자 정의 메서드:개발자에 의해 만들어지는 함수
+	public static void main(String[] args)//메소드 영역-절차 지향 문법 적용 단위
+	{
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+		//괄호 붙어있으면 대부분 함수
+
+	}
+
+}
